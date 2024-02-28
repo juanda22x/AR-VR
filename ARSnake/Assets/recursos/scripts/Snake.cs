c4e58b2848691f7c188e9bac9f6078add3d9f416
juanda22x <juan.castillor@upb.edu.co>
2024-02-28 13:18:00 -0500

    v2clase

--- ARSnake/Assets/recursos/scripts/Snake.cs
+++ ARSnake/Assets/recursos/scripts/Snake.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class Snake2 : MonoBehaviour
+public class Snake : MonoBehaviour
 {
+
     // Settings
     public float MoveSpeed = 5;
     public float SteerSpeed = 180;
     public float BodySpeed = 5;
     public int Gap = 10;
+
 
     // References
     public GameObject BodyPrefab;
@@ -18,13 +19,6 @@ public class Snake2 : MonoBehaviour
     // Lists
     private List<GameObject> BodyParts = new List<GameObject>();
     private List<Vector3> PositionsHistory = new List<Vector3>();
-
-    // UI Buttons
-    public Button leftButton;
-    public Button rightButton;
-
-    private bool isTurningLeft = false;
-    private bool isTurningRight = false;
 
     // Start is called before the first frame update
     void Start()
@@ -35,28 +29,18 @@ public class Snake2 : MonoBehaviour
         GrowSnake();
         GrowSnake();
         GrowSnake();
-
-
-        // Assign button click events
-        leftButton.onClick.AddListener(TurnLeft);
-        rightButton.onClick.AddListener(TurnRight);
     }
 
     // Update is called once per frame
     void Update()
     {
+
         // Move forward
         transform.position += transform.forward * MoveSpeed * Time.deltaTime;
 
-        // Turn left or right
-        if (isTurningLeft)
-        {
-            transform.Rotate(Vector3.up * -1 * SteerSpeed * Time.deltaTime);
-        }
-        else if (isTurningRight)
-        {
-            transform.Rotate(Vector3.up * SteerSpeed * Time.deltaTime);
-        }
+        // Steer
+        float steerDirection = Input.GetAxis("Horizontal"); // Returns value -1, 0, or 1
+        transform.Rotate(Vector3.up * steerDirection * SteerSpeed * Time.deltaTime);
 
         // Store position history
         PositionsHistory.Insert(0, transform.position);
@@ -85,27 +69,15 @@ public class Snake2 : MonoBehaviour
         GameObject body = Instantiate(BodyPrefab);
         BodyParts.Add(body);
     }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Apple"))
         {
+
             Destroy(other.gameObject);
             GrowSnake();
-            Instantiate(enemy, new Vector3(Random.Range(-1.0f, 1.0f), 0f, 0f), Quaternion.identity);
+            Instantiate(enemy, new Vector3(Random.Range(-10.0f, 10.0f), 0f, 0f), Quaternion.identity);
         }
-    }
 
-    // Button click events
-    private void TurnLeft()
-    {
-        isTurningLeft = true;
-        isTurningRight = false;
-    }
-
-    private void TurnRight()
-    {
-        isTurningLeft = false;
-        isTurningRight = true;
     }
 }
